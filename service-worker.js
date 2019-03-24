472a8f915d170ddc847581330732e103f58ff78b
Joseph Young <jy14898@my.bristol.ac.uk>
2019-03-24 19:27:39 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8602ddafc80e9081a1808a516a32c700.js"
+  "/precache-manifest.eb4a0656d53c96e6785878bf84270cbb.js"
 );
 
 workbox.clientsClaim();
