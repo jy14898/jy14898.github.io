c0defc5f6198194327e458acb4835edb860b5e83
Joseph Young <jy14898@my.bristol.ac.uk>
2019-03-25 23:55:49 +0000

    .

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import styled from 'styled-components/macro';
 
 import { GlobalStyle } from './theme/globalStyle';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faStackExchange, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
 const AppWrapper = styled.div`
-    margin: 100px auto;
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+`;
+
+const Idk = styled.div`
     width: 400px;
 `;
 
@@ -18,14 +26,28 @@ const Headline = styled.div`
   text-align: center;
 `;
 
+const Social = styled.div`
+    margin-top: 10px;
+    font-size:2em;
+    display: flex;
+    justify-content: space-evenly;
+`;
+
 class App extends Component {
   render() {
     return (
         <>
             <GlobalStyle />
             <AppWrapper>
-                <Headline>welcome to my website</Headline>
-                I might put stuff here later
+                <Idk>
+                    <Headline>welcome to my website</Headline>
+                    I might put stuff here later
+                    <Social>
+                        <a href="https://github.com/jy14898"><FontAwesomeIcon icon={faGithub}/></a>
+                        <a href="https://stackexchange.com/users/7246868/joseph-young"><FontAwesomeIcon icon={faStackExchange}/></a>
+                        <a href="https://www.youtube.com/channel/UCgPjQ2V3APd4wv7myjEZoBA"><FontAwesomeIcon icon={faYoutube}/></a>
+                    </Social>
+                </Idk>
             </AppWrapper>
         </>
     );
