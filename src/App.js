4394c3fbd441e150f5976e6d532fc3e7dba92e4d
Joseph Young <jy14898@my.bristol.ac.uk>
2019-06-14 13:27:49 +0100

    Forgot codepen link

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
                         <a href="https://github.com/jy14898"><FontAwesomeIcon icon={faGithub}/></a>
                         <a href="https://stackexchange.com/users/7246868/joseph-young"><FontAwesomeIcon icon={faStackExchange}/></a>
                         <a href="https://www.youtube.com/channel/UCgPjQ2V3APd4wv7myjEZoBA"><FontAwesomeIcon icon={faYoutube}/></a>
-                        <a href=""><FontAwesomeIcon icon={faCodepen}/></a>
+                        <a href="https://codepen.io/jy14898"><FontAwesomeIcon icon={faCodepen}/></a>
                         <a href="https://orcid.org/0000-0003-1330-0515"><i class="ai ai-orcid ai-1x"></i></a>
                     </Social>
                 </Idk>
