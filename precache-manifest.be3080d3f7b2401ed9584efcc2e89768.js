e680373a7e91c2a74ba6d4f2a1d67fdfdfcffe88
Joseph Young <jy14898@my.bristol.ac.uk>
2019-08-15 11:16:24 +0100

    Updates

---
+++ precache-manifest.be3080d3f7b2401ed9584efcc2e89768.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187ba1281820707554978aa52005c43d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aefe45e1fb194dcddf28",
+    "url": "/static/css/2.92c2e21d.chunk.css"
+  },
+  {
+    "revision": "aefe45e1fb194dcddf28",
+    "url": "/static/js/2.cf313339.chunk.js"
+  },
+  {
+    "revision": "b8b3239314e5c5d619de",
+    "url": "/static/js/main.5bc5470d.chunk.js"
+  },
+  {
+    "revision": "1bf83a678ff177b39060",
+    "url": "/static/js/runtime~main.c93da913.js"
+  },
+  {
+    "revision": "1afcbd6a1c454e7b7f0bc58155558b9d",
+    "url": "/static/media/academicons.1afcbd6a.ttf"
+  },
+  {
+    "revision": "2552b354cabf01815628a244fde0636e",
+    "url": "/static/media/academicons.2552b354.woff"
+  },
+  {
+    "revision": "406b38d29cffe8ffcb7962faa2e095ef",
+    "url": "/static/media/academicons.406b38d2.svg"
+  },
+  {
+    "revision": "ea3755c311d0678ca4f041912a9039d6",
+    "url": "/static/media/academicons.ea3755c3.eot"
+  }
+]);
