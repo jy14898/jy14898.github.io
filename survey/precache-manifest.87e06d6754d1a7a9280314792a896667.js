cfc0aced502037f34cbcd844516cbde1f4b2c3ef
Joseph Young <jy14898@my.bristol.ac.uk>
2020-01-10 16:15:51 +0000

    .

---
+++ survey/precache-manifest.87e06d6754d1a7a9280314792a896667.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b705b95b7b3bafe8ee16ca5c9d6c47",
+    "url": "/survey/index.html"
+  },
+  {
+    "revision": "8efae51dacccd2275d62",
+    "url": "/survey/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "205e20328555894db486",
+    "url": "/survey/static/css/main.f7c6cf00.chunk.css"
+  },
+  {
+    "revision": "8efae51dacccd2275d62",
+    "url": "/survey/static/js/2.71d3f592.chunk.js"
+  },
+  {
+    "revision": "57298b49bd6ed2c03c2216d2b1a7ee91",
+    "url": "/survey/static/js/2.71d3f592.chunk.js.LICENSE"
+  },
+  {
+    "revision": "205e20328555894db486",
+    "url": "/survey/static/js/main.7c973ae5.chunk.js"
+  },
+  {
+    "revision": "3a225f66c03ad2197ff0",
+    "url": "/survey/static/js/runtime-main.705cd21a.js"
+  },
+  {
+    "revision": "09e1b18013de988ea51743b49a9d2ca3",
+    "url": "/survey/static/media/keys.09e1b180.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/survey/static/media/logo.5d5d9eef.svg"
+  }
+]);
